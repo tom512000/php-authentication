d803c38b87d1a38fc18594888f4c0b5c180daef3
siko0001 <tom.sikora@etudiant.univ-reims.fr>
2023-09-27 18:13:44 +0200

    5. Gestion des données : classe « User »

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Entity;
 
 use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
 
 class User
 {
@@ -15,54 +16,54 @@ class User
     private string $phone;
 
     /**
-     * @return int
+     * @param string $login    Login de l'utilisateur
+     * @param string $password Mot de passe en clair
+     *
+     * @throws EntityNotFoundException
      */
+    public static function findByCredentials(string $login, string $password): User
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT id, lastName, firstName, login, phone
+            FROM user
+            WHERE login = :login
+            AND sha512pass = SHA2(:password, 512)
+        SQL
+        );
+        if (!$stmt->execute([':login' => $login, ':password' => $password])) {
+            throw new EntityNotFoundException("L'utilisateur n'existe pas !");
+        }
+
+        if (($utilisateur = $stmt->fetchObject(User::class)) === false) {
+            throw new EntityNotFoundException("Impossible de charger l'utilisateur en mémoire !");
+        }
+
+        return $utilisateur;
+    }
+
     public function getId(): int
     {
         return $this->id;
     }
 
-    /**
-     * @return string
-     */
     public function getLastName(): string
     {
         return $this->lastName;
     }
 
-    /**
-     * @return string
-     */
     public function getFirstName(): string
     {
         return $this->firstName;
     }
 
-    /**
-     * @return string
-     */
     public function getLogin(): string
     {
         return $this->login;
     }
 
-    /**
-     * @return string
-     */
     public function getPhone(): string
     {
         return $this->phone;
-    }
-
-    public function findByCredentials(string $login, string $password) : User
-    {
-        $stmt = MyPDO::getInstance()->prepare(
-            <<<'SQL'
-            SELECT login, password
-            FROM user
-        SQL
-        );
-        $stmt->execute();
-        $stmt->setFetchMode(\PDO::FETCH_CLASS, User::class);
     }
 }
---
+++ src/Html/UserProfile.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Html;
+
+use Entity\User;
+
+class UserProfile
+{
+    /** @var User Utilisateur */
+    private User $user;
+
+    use StringEscaper;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function toHtml(): string
+    {
+        return <<<HTML
+        <p>Nom<br>&nbsp;&nbsp;&nbsp;&nbsp;{$this->escapeString($this->user->getFirstName())}</p>
+        <p>Prénom<br>&nbsp;&nbsp;&nbsp;&nbsp;{$this->escapeString($this->user->getLastName())}</p>
+        <p>Login<br>&nbsp;&nbsp;&nbsp;&nbsp;{$this->escapeString($this->user->getLogin())}[{$this->user->getId()}]</p>
+        <p>Téléphone<br>&nbsp;&nbsp;&nbsp;&nbsp;{$this->escapeString($this->user->getPhone())}</p>
+    HTML;
+    }
+}
