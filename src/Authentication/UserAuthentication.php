5baba5416a13f7f346888597595a7510bb0d9de0
siko0001 <tom.sikora@etudiant.univ-reims.fr>
2023-10-12 10:41:48 +0200

    7.1. Modification des données de l'avatar

--- src/Authentication/UserAuthentication.php
+++ src/Authentication/UserAuthentication.php
@@ -41,8 +41,9 @@ class UserAuthentication
     /**
      * Récupère l'utilisateur de la session, vérifie si cet utilisateur est une instance de User et l'affecte à l'attribut $user et à la clé de session.
      * Retourne également l'utilisateur.
-     * 
-     * @return User Utilisateur (User) de la session.
+     *
+     * @return User utilisateur (User) de la session
+     *
      * @throws NotLoggedInException
      */
     public function getUserFromSession(): User
@@ -50,20 +51,22 @@ class UserAuthentication
         Session::start();
         if (isset($_SESSION[self::SESSION_KEY][self::SESSION_USER_KEY])) {
             $utilisateur = $_SESSION[self::SESSION_KEY][self::SESSION_USER_KEY];
-            if ($utilisateur instanceof User){
+            if ($utilisateur instanceof User) {
                 $this->setUser($utilisateur);
+
                 return $utilisateur;
             }
         }
-        throw new NotLoggedInException("Aucun utilisateur dans la session !");
+        throw new NotLoggedInException('Aucun utilisateur dans la session !');
     }
 
     /**
      * Retourne le formulaire de connexion en HTML.
-     * 
-     * @param string $action URL qui traite l'envoi du formulaire.
-     * @param string $submitText Valeur du bouton de connexion.
-     * @return string Code HTML du formulaire.
+     *
+     * @param string $action     URL qui traite l'envoi du formulaire
+     * @param string $submitText valeur du bouton de connexion
+     *
+     * @return string code HTML du formulaire
      */
     public function loginForm(string $action, string $submitText = 'OK'): string
     {
@@ -82,7 +85,8 @@ class UserAuthentication
     /**
      * Récupère l'utilisateur dans la BD à partir des données du formulaire.
      *
-     * @return User L'instance User de l'utilisateur s'il a été trouvé dans la BD.
+     * @return user L'instance User de l'utilisateur s'il a été trouvé dans la BD
+     *
      * @throws SessionException
      */
     public function getUserFromAuth(): User
@@ -98,8 +102,9 @@ class UserAuthentication
 
     /**
      * Vérifie si la clé de la session de l'utilisateur est déclarée et si elle contient bien une instance de User.
-     * 
-     * @return bool true si la clé de la session de l'utilisateur existe et si elle contient une instance de User, false sinon.
+     *
+     * @return bool true si la clé de la session de l'utilisateur existe et si elle contient une instance de User, false sinon
+     *
      * @throws SessionException
      */
     public function isUserConnected(): bool
@@ -116,10 +121,11 @@ class UserAuthentication
 
     /**
      * Retourne le formulaire de déconnexion en HTML.
-     * 
-     * @param string $action URL qui traite l'envoi du formulaire.
-     * @param string $text Valeur du label du formulaire.
-     * @return string Code HTML du formulaire.
+     *
+     * @param string $action URL qui traite l'envoi du formulaire
+     * @param string $text   valeur du label du formulaire
+     *
+     * @return string code HTML du formulaire
      */
     public function logoutForm(string $action, string $text): string
     {
@@ -135,7 +141,7 @@ class UserAuthentication
 
     /**
      * Efface la donnée de session associée à l'utilisateur et l'attribut $user si une valeur « 'logout' » de formulaire est reçue.
-    */
+     */
     public function logoutIfRequested(): void
     {
         try {
@@ -150,8 +156,9 @@ class UserAuthentication
 
     /**
      * Retourne l'instance de User stockée dans l'attribut $user.
-     * 
-     * @return User Utilisateur stocké dans l'attribut $user.
+     *
+     * @return User utilisateur stocké dans l'attribut $user
+     *
      * @throws NotLoggedInException
      */
     public function getUser(): User
@@ -165,8 +172,9 @@ class UserAuthentication
 
     /**
      * Affecte une instance de User à la clé de la session de l'utilisateur et à l'attribut $user.
-     * 
-     * @param User $user Instance de User à stocker.
+     *
+     * @param User $user instance de User à stocker
+     *
      * @throws SessionException
      */
     protected function setUser(User $user): void
--- src/Entity/UserAvatar.php
+++ src/Entity/UserAvatar.php
@@ -9,10 +9,14 @@ use Entity\Exception\EntityNotFoundException;
 
 class UserAvatar
 {
+    // Attribut permettant de stocker l'id de l'avatar.
     private int $id;
+    // Attribut permettant de stocker soit un avatar (chaine de caractères), soit null.
     private ?string $avatar;
 
     /**
+     * Retourne l'attribut id de l'instance de UserAvatar.
+     *
      * @return int
      */
     public function getId(): int
@@ -21,7 +25,9 @@ class UserAvatar
     }
 
     /**
-     * @return string|null
+     * Retourne l'attribut avatar de l'instance de UserAvatar.
+     *
+     * @return string|null soit avatar (chaine de caractères), soit null
      */
     public function getAvatar(): ?string
     {
@@ -29,11 +35,42 @@ class UserAvatar
     }
 
     /**
+     * Retoune l'attribut avatar de l'instance de UserAvatar.
+     *
+     * @param string|null $avatar Nouvel avatar
+     *
+     * @return UserAvatar Instance après changement de l'attribut avatar
+     */
+    public function setAvatar(?string $avatar): UserAvatar
+    {
+        $this->avatar = $avatar;
+
+        return $this;
+    }
+
+    public function save(): UserAvatar
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            UPDATE user
+            SET avatar = :avatar
+            WHERE id = :id
+        SQL
+        );
+
+        if (!$stmt->execute([':avatar' => $this->avatar, ':id' => $this->id])) {
+            throw new EntityNotFoundException("L'utilisateur n'existe pas !");
+        }
+
+        return $this;
+    }
+
+    /**
      * Recherche dans la base de données l'utilisateur dont l'identifiant est passé en paramètre.
      *
-     * @param int $userId
-     * @return UserAvatar Avatar trouvé dans la base de données.
-     * @throws EntityNotFoundException Si l'avatar n'a pas été trouvé.
+     * @return UserAvatar avatar trouvé dans la base de données
+     *
+     * @throws EntityNotFoundException si l'avatar n'a pas été trouvé
      */
     public static function findById(int $userId): UserAvatar
     {
